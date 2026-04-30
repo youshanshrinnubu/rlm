7fdb4a0b49b9d0a2cce0ca9118ada16f878ceda5
Youshan Shrinnubu <youshanshrinnubu@users.noreply.github.com>
2026-04-30 09:23:46 +0800

    expose RLMEvaluator in public API for easier access in experiment scripts

--- src/rlm/__init__.py
+++ src/rlm/__init__.py
@@ -5,6 +5,9 @@ reinforcement learning techniques.
 
 Personal fork: using this to experiment with RLHF on smaller models.
 See my notes in /docs/experiments for details.
+
+Note: Added RLMEvaluator to the public API since I use it frequently
+in my experiment scripts and was tired of importing it directly.
 """
 
 from importlib.metadata import PackageNotFoundError, version
@@ -19,11 +22,13 @@ __license__ = "MIT"
 
 from rlm.agent import RLMAgent
 from rlm.config import RLMConfig
+from rlm.evaluator import RLMEvaluator
 from rlm.trainer import RLMTrainer
 
 __all__ = [
     "RLMAgent",
     "RLMConfig",
+    "RLMEvaluator",
     "RLMTrainer",
     "__version__",
 ]
